81d72199d8ce8fdd44f794144dea4ecfca7ea765
Nic McPhee <mcphee@morris.umn.edu>
2015-03-14 20:41:20 -0400

    Added run_uuid to the node output file.

--- scripts/split_for_batch_import.rb
+++ scripts/split_for_batch_import.rb
@@ -15,9 +15,9 @@ require 'securerandom'
 # and it will generate two new files: data6_nodes.csv and data6_edges.csv
 
 # The headers for replace-space-with-newline are:
-#   uuid,generation,location,parent-uuids,genetic-operators,
-#   push-program-size,plush-genome-size,push-program,plush-genome,
-#   total-error,TC0,TC1,TC2,...,TC199
+#   uuid,generation,location,parent_uuids,genetic_operators,
+#   push_program_size,plush_genome_size,push_program,plush_genome,
+#   total_error,TC0,TC1,TC2,...,TC199
 # We need to replace all the dashes in the headers with underscores,
 # and it would be good to glob all the errors into an array instead of
 # individual columns.
@@ -35,7 +35,7 @@ def dashes_to_newlines(str)
 end
 
 printed_headers = false
-CSV.open(node_file, "wb", :headers => true, :write_headers => true) do |nodes|
+CSV.open(node_file, "wb") do |nodes|
   CSV.open(edge_file, "wb") do |edges|
     num_rows = 0
     CSV.open(input_file, "r",
@@ -44,12 +44,11 @@ CSV.open(node_file, "wb", :headers => true, :write_headers => true) do |nodes|
     :converters => :numeric) do |inputs|
       inputs.each do |row|
         if not printed_headers
-          nodes << inputs.headers
+          nodes << (inputs.headers + ["run_uuid"])
           printed_headers = true
         end
+        row["run_uuid"] = run_uuid
         nodes << row
-        puts "Row!"
-        #                  p row
       end
     end
   end
