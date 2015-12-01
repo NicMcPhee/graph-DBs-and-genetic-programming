c19c1fe37b986ca9a190f1926aeb9eb04c49ba64
casal033 <casal033@morris.umn.edu>
2015-11-30 19:06:11 -0600

    Commint for 11/30 work

--- gremlin_scripts/LoadNodes.groovy
+++ gremlin_scripts/LoadNodes.groovy
@@ -1,9 +1,4 @@
-graph = TitanFactory.build().
-set("storage.backend", "berkeleyje").
-set("storage.directory", "/tmp/graph").
-set("index.search.backend", "elasticsearch").
-set("index.search.directory", "/tmp/searchindex").
-open();
+graph = TitanFactory.open("db.properties");
 
 
 //g = graph.traversal()
@@ -23,19 +18,31 @@ new File(nodeCSV).splitEachLine(",") { fields ->
 	}
 }
 
+println("# of nodes = " + graph.traversal().V().count())
+
+/*
+
 graph.tx().rollback()  //Never create new indexes while a transaction is active
 mgmt = graph.openManagement()
-mgmt.set("index.search.elasticsearch.client-only", false)
-mgmt.set("index.search.elasticsearch.local-mode", true)
+//mgmt.set("index.search.elasticsearch.client-only", false)
+//mgmt.set("index.search.elasticsearch.local-mode", true)
 
 uuid = mgmt.makePropertyKey('uuid').dataType(String.class).cardinality(Cardinality.SINGLE).make()
 generation = mgmt.makePropertyKey('generation').dataType(Integer.class).cardinality(Cardinality.SINGLE).make()
 total_error = mgmt.makePropertyKey('total_error').dataType(Float.class).cardinality(Cardinality.SINGLE).make()
 mgmt.buildIndex('uuidGenerationTotalError', Vertex.class).addKey(uuid).addKey(generation).addKey(total_error).buildMixedIndex("search")
+println("About to commit")
 mgmt.commit()
 //Wait for the index to become available
+graph = TitanFactory.open("db.properties")
+mgmt = graph.openManagement()
+println("About to wait for index")
 mgmt.awaitGraphIndexStatus(graph, 'uuidGenerationTotalError').call()
 //Reindex the existing data
+println("About to re-open mgmt")
 mgmt = graph.openManagement()
+println("About to update index")
 mgmt.updateIndex(mgmt.getGraphIndex("uuidGenerationTotalError"), SchemaAction.REINDEX).get()
 mgmt.commit()
+
+*/
