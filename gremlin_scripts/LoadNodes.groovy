20c975e8425dba9297a3beedc0b74bb0a303a216
casal033 <casal033@morris.umn.edu>
2015-12-07 19:14:22 -0600

    comminting work from 12/7

--- gremlin_scripts/LoadNodes.groovy
+++ gremlin_scripts/LoadNodes.groovy
@@ -3,9 +3,15 @@ graph = TitanFactory.open("db.properties");
 
 //g = graph.traversal()
 
+// nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes_100.csv"
 nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes.csv"
 
 println("We're in the script")
+mgmt = graph.openManagement()
+mgmt.makePropertyKey('uuid').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+mgmt.makePropertyKey('generation').dataType(Integer.class).cardinality(Cardinality.SINGLE).make()
+mgmt.makePropertyKey('total_error').dataType(Float.class).cardinality(Cardinality.SINGLE).make()
+mgmt.commit()
 
 count = 0
 new File(nodeCSV).splitEachLine(",") { fields ->
@@ -18,31 +24,37 @@ new File(nodeCSV).splitEachLine(",") { fields ->
 	}
 }
 
-println("# of nodes = " + graph.traversal().V().count())
+graph.tx().commit()
 
-/*
-
-graph.tx().rollback()  //Never create new indexes while a transaction is active
+graph.tx().rollback()
+//println("Graph open? "+ graph.isOpen())
 mgmt = graph.openManagement()
-//mgmt.set("index.search.elasticsearch.client-only", false)
-//mgmt.set("index.search.elasticsearch.local-mode", true)
-
-uuid = mgmt.makePropertyKey('uuid').dataType(String.class).cardinality(Cardinality.SINGLE).make()
-generation = mgmt.makePropertyKey('generation').dataType(Integer.class).cardinality(Cardinality.SINGLE).make()
-total_error = mgmt.makePropertyKey('total_error').dataType(Float.class).cardinality(Cardinality.SINGLE).make()
+//println("Graph open? open manage "+ graph.isOpen())
+uuid = mgmt.getPropertyKey('uuid')
+generation = mgmt.getPropertyKey('generation')
+total_error = mgmt.getPropertyKey('total_error')
 mgmt.buildIndex('uuidGenerationTotalError', Vertex.class).addKey(uuid).addKey(generation).addKey(total_error).buildMixedIndex("search")
 println("About to commit")
 mgmt.commit()
-//Wait for the index to become available
-graph = TitanFactory.open("db.properties")
-mgmt = graph.openManagement()
+graph.tx().commit()
+
 println("About to wait for index")
-mgmt.awaitGraphIndexStatus(graph, 'uuidGenerationTotalError').call()
-//Reindex the existing data
-println("About to re-open mgmt")
+// Block until the SchemaStatus transitions from INSTALLED to REGISTERED
+mgmt.awaitGraphIndexStatus(graph, 'uuidGenerationTotalError').status(SchemaStatus.REGISTERED).call()
+
+// Reindex using TitanManagement
 mgmt = graph.openManagement()
+println("About to get graph the index")
+i = mgmt.getGraphIndex('uuidGenerationTotalError')
 println("About to update index")
-mgmt.updateIndex(mgmt.getGraphIndex("uuidGenerationTotalError"), SchemaAction.REINDEX).get()
+mgmt.updateIndex(i, SchemaAction.REINDEX)
+println("About to commit")
 mgmt.commit()
 
-*/
+// Enable the index
+println("About to Enable the index")
+mgmt.awaitGraphIndexStatus(graph, 'uuidGenerationTotalError').status(SchemaStatus.ENABLED).call()
+//mgmt.awaitGraphIndexStatus(graph, 'uuidGenerationTotalError').call()
+println("About to close the graph")
+graph.close()
+
