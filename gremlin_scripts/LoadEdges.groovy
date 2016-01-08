39be1bfabbb13f703e19f4a5031cc7fd53d4246d
casal033 <casal033@morris.umn.edu>
2016-01-08 13:50:23 -0600

    Forgot to add files...

--- gremlin_scripts/LoadEdges.groovy
+++ gremlin_scripts/LoadEdges.groovy
@@ -19,29 +19,29 @@ getOrCreate = { uuid ->
   }
 }
 */  
-
+start = System.currentTimeMillis()
 println("Getting the file")
 edgeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_edges.csv"
 
 println("Looping the file")
 counter = 0
-theCount = 0
 new File(edgeCSV).splitEachLine(",") { fielder ->
-	++counter
-    if (counter > 1) { 
-		if(theCount == 10000){
+	println("Count = ${counter}")
+    if (counter > 0) { 
+		if((counter % 10000) == 0){
 			graph.tx().commit()
-			theCount = 0;
-			println("Commiting!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			println("Commiting at: "+counter)
 		}
 		//println("This is what we get "+fielder[0])
    		parentVertex = g.V().has("uuid", fielder[0]).next()
 		childVertex = g.V().has("uuid", fielder[1]).next()
 	 	theEdge = parentVertex.addEdge('parent_of', childVertex)
-		println("Added the: "+counter+" Edge: "+theEdge)
+		//println("Added the: "+counter+" Edge: "+theEdge + " in " + (System.currentTimeMillis() - start))
 	}
+	++counter
 }
   
 println("Total added verticies: "+counter)
+println "Loading took (ms): " + (System.currentTimeMillis() - start)
 println("Commiting")
-graph.tx.commit()
+graph.tx().commit()
--- gremlin_scripts/LoadNodes.groovy
+++ gremlin_scripts/LoadNodes.groovy
@@ -1,17 +1,19 @@
 //nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes_100.csv"
 //nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes2000.csv"
 //nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes.csv"
-
+//storage.batch-loading=true //for batch graph, use in properties
 graph = TitanFactory.open('./db.properties')
+
 mgmt = graph.openManagement()
 run_uuid = mgmt.makePropertyKey("run_uuid").dataType(String.class).make()
 uuid = mgmt.makePropertyKey("uuid").dataType(String.class).make()		
 generation = mgmt.makePropertyKey("generation").dataType(Integer.class).make()
-ocation = mgmt.makePropertyKey("location").dataType(Integer.class).make()
+location = mgmt.makePropertyKey("location").dataType(Integer.class).make()
 total_error = mgmt.makePropertyKey("total_error").dataType(Float.class).make()
-numSelections = mgmt.makePropertyKey("numSelections").dataType(Float.class).make()
-numChildren = mgmt.makePropertyKey("numChildren").dataType(Float.class).make()
-uuidGenerationTotalError = mgmt.buildIndex('uuidGenerationTotalError', Vertex.class).addKey(generation).addKey(total_error).buildMixedIndex("search")
+numSelections = mgmt.makePropertyKey("numSelections").dataType(Integer.class).make()
+numChildren = mgmt.makePropertyKey("numChildren").dataType(Integer.class).make()
+uuidIndex = mgmt.buildIndex("uuidIndex",  Vertex.class).addKey(uuid).unique().buildCompositeIndex()
+uuidGenerationTotalError = mgmt.buildIndex('uuidGenerationTotalError', Vertex.class).addKey(uuid).addKey(generation).addKey(total_error).buildMixedIndex("search")
 mgmt.commit()
 
 println("Done with setting keys.")
@@ -19,21 +21,18 @@ start = System.currentTimeMillis()
 println("We're in the parse section!")
 // Adding all verticies to graph
 theCount = 0
-count = 0
 nodeCSV = "/Research/autoconstruction_neo4j/replace-space-with-newline__d_nodes.csv"
 new File(nodeCSV).splitEachLine(",") { fields ->
 println("Count = ${theCount}")
     if (theCount > 0) { 
-		if(count == 10000){
+		if((theCount % 10000) == 0){
 			graph.tx().commit()
-			count = 0;
-			println("Commiting!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			println("Commiting at: "+theCount)
 		}
 		graph.addVertex("run_uuid", fields[1], "uuid", fields[2], "generation", fields[3].toInteger(), 
 				"location", fields[4].toInteger(), "total_error", fields[5].toFloat(), 
 				"numSelections", fields[6].toInteger(), "numChildren", fields[7].toInteger())
 	}
-	++count
 	++theCount
 }
 graph.tx().commit()
