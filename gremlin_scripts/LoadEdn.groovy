e160123ace9270a9143ef7c542785601e84fb62c
Laverne Schrock <schr1230@morris.umn.edu>
2016-07-13 16:58:37 -0500

    Remove unused functions and comments from LoadEDN

--- gremlin_scripts/LoadEdn.groovy
+++ gremlin_scripts/LoadEdn.groovy
@@ -19,25 +19,6 @@ debugStatus = { str ->
   }
 }
 
-// Based on an algorithm by maythesource.com:
-// http://stackoverflow.com/a/15905916
-fastSplit = { s ->
-  words = []
-  boolean notInsideComma = true
-  int start = 0
-  int end = 0
-  for (int i=0; i<s.length()-1; ++i) {
-    if (s.charAt(i) == ',' && notInsideComma) {
-      words += [s.substring(start, i)]
-      start = i+1
-    } else if (s.charAt(i) == '"') {
-      notInsideComma = !notInsideComma;
-    }
-  }
-  words += [s.substring(start)]
-  return words
-}
-
 createPropertiesAndKeys = { graph ->
   mgmt = graph.openManagement()
 
@@ -86,99 +67,6 @@ createPropertiesAndKeys = { graph ->
   //println("Done with setting keys.")
 }
 
-/*
-parseCsvFile = { graph, zippedCsvFile, runUUID ->
-  g = graph.traversal()
-
-  //println("We're in the parse section!")
-
-  fileStream = new FileInputStream(zippedCsvFile)
-  gzipStream = new GZIPInputStream(fileStream)
-  inputReader = new InputStreamReader(gzipStream)
-  reader = new BufferedReader(inputReader)
-
-  theCount = 0 // How many rows have we read?
-  successful = false // Was this run successful (i.e., zero error?)
-  maxGen = 0 // The last (& largest) generation number in this run
-  while ((line = reader.readLine()) != null) {
-    if (theCount % 10000 == 0) {
-      println("Count = ${theCount}")
-    }
-    // The first line is the headers, which we currently ignore. We really
-    // should read them and use them to drive a lot of the stuff that we end
-    // up fiddling by hand below.
-    if (theCount > 0) {
-      fields = this.fastSplit(line)
-      // This only makes sense if we're using is-random-replacement
-      // (and it's in location 9).
-      
-      // if (fields[9] == "") { is_rand = true }
-      // else { is_rand = fields[9].toBoolean()}
-      
-
-      // The case to int here will be bad if the error values are ever
-      // actually floating point values, but it works for the integer values
-      // in the problems we're currently looking at.
-      error_vector_values = fields[10..-1].collect { (int) (it.toFloat()) }
-
-      errors_with_indices = error_vector_values.withIndex() // [[a, 0], [b, 1], [c, 2], ...]
-      num_zero_errors_even_indices = errors_with_indices.findAll { it[1] % 2 == 0 && it[0] == 0 }.size
-      num_zero_errors_odd_indices = errors_with_indices.findAll { it[1] % 2 == 1 && it[0] == 0 }.size
-      // Evens are printing tests
-      percent_zeros_even_indices = num_zero_errors_even_indices * 1.0 / (error_vector_values.size / 2)
-      // Odds are returning tests
-      percent_zeros_odd_indices = num_zero_errors_odd_indices * 1.0 / (error_vector_values.size / 2)
-
-      errors = fields[10..-1].join(",")
-
-      if((theCount % 1000) == 0){
-        println("Commiting at: "+theCount)
-        graph.tx().commit()
-      }
-      // Remember to change this to fields[9] when working with non autoconstuctive runs!
-      total_error = fields[9].toFloat()
-      if (total_error == 0) {
-        successful = true;
-      }
-      gen = fields[1].toInteger()
-      if (gen > maxGen){
-        maxGen = gen;
-      }
-
-      newVertex = graph.addVertex(label, "individual", "run_uuid", runUUID,
-      "uuid", fields[0],
-      "generation", fields[1].toInteger(), "location", fields[2].toInteger(),
-      "genetic_operators", fields[4],
-      "plush_genome_size", fields[6],
-      "plush_genome", fields[8],
-      // For autoconstruction runs
-      //"total_error", total_error, "is_random_replacement", is_rand, "error_vector", errors,
-      "total_error", total_error, "error_vector", errors,
-      "percent_zero_errors_evens", percent_zeros_even_indices,
-      "percent_zero_errors_odds", percent_zeros_odd_indices)
-
-      if (fields[3].length() > 5) {
-        motherUuid = fields[3][4..39]
-        // println "<" + motherUuid + "> ::: <" + fatherUuid + ">"
-        mother = g.V().has("uuid", motherUuid).next()
-        motherEdge = mother.addEdge('parent_of', newVertex)
-        motherEdge.property("parent_type", "mother")
-        if (fields[3].length() > 48) {
-          fatherUuid = fields[3][45..-5]
-          father = g.V().has("uuid", fatherUuid).next()
-          fatherEdge = father.addEdge('parent_of', newVertex)
-          fatherEdge.property("parent_type", "father")
-        }
-      }
-    }
-    ++theCount
-  }
-  reader.close()
-  graph.tx().commit()
-
-  return [maxGen, successful]
-}
-*/
 
 /* Takes an individual represented as a map from parsed EDN.
  * Takes a graph
@@ -399,29 +287,6 @@ addLevenshteinDistances = { graph, maxGen ->
   }
 }
 
-addMinimalContributionProperty = { graph, maxGen ->
-  g = graph.traversal()
-
-  (0..maxGen).each { gen ->
-    g.V().has('generation', gen).sideEffect { node_traverser ->
-      child = node_traverser.get()
-      child_size = child.value('plush_genome_size')
-      parent_edges = child.edges(Direction.IN).sort{ it.value('DL_dist') }
-      unique_parents = parent_edges.collect{ it.outVertex() }.unique()
-      if (unique_parents.size() == 2) {
-        parent_edges = child.edges(Direction.IN).sort{ it.value('DL_dist') }
-        distances = parent_edges.collect{ it.value('DL_dist') }
-        parent_edges = child.edges(Direction.IN).sort{ it.value('DL_dist') }
-        if (distances[0] / 10 < 0.2*child_size || distances[1] >= 2 * distances[0]) {
-          // println "${child_size}, ${distances[0]}, and ${distances[1]}"
-          parent_edges[1].property('minimal_contribution', true)
-        }
-      }
-    }.iterate()
-    graph.tx().commit()
-    println gen
-  }
-}
 
 addRunNode = { graph, runUUID, runFileName, successful, maxGen ->
   runVertex = graph.addVertex(label, "run", "run_uuid", runUUID, "data_file", runFileName, "successful", successful, "max_generation", maxGen)
@@ -443,7 +308,6 @@ loadEdn = { propertiesFileName, ednDataFile ->
   (maxGen, successful, runUUID) = parseEdnFile(graph, ednDataFile)
 
   // TODO addLevenshteinDistances(graph, maxGen)
-  // TODO addMinimalContributionProperty(graph, maxGen)
 
   debugStatus("adding num selections and num children")
   addNumSelections(graph, maxGen)
