10053404462a15bce374c0dd147b1a5077f1955e
Laverne Schrock <schr1230@morris.umn.edu>
2016-07-28 16:32:46 -0500

    Add license info for the DL-distance function

--- gremlin_scripts/LoadEdn.groovy
+++ gremlin_scripts/LoadEdn.groovy
@@ -370,6 +370,28 @@ genome_items = { node ->
 
 damerauLevenshteinDistance = { ArrayList s, ArrayList t ->
 
+  /*
+   * This function is a straight foward ADAPTION of the helper
+   * method in org.codehaus.groovy.runtime.MethodRankHelper
+   *
+   * That method is copyright 2003-2016 The Apache Software Foundation.
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this function except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   *
+   * The author was Hjalmar Ekengren, and the implemenation was based on
+   * Chas Emerick's implementation of Levenshtein Distance implementation
+   * for jakarta commons.
+   */
+
   int DL_SUBSTITUTION = 1
   int DL_DELETE = 1 //This is also the cost for a insert
   int DL_TRANSPOSITION = 1
