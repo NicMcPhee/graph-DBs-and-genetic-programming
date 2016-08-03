6cc78a0d651b2043dbabeee570808989656a6cc4
Laverne Schrock <schr1230@morris.umn.edu>
2016-08-03 17:36:02 -0500

    Start implementing comparisons with distant ancestors
    This code contains large duplicates of the code in the previous function
    and should be refactored, but it mostly does what we want.

--- gremlin_scripts/sketchParent.groovy
+++ gremlin_scripts/sketchParent.groovy
@@ -86,3 +86,119 @@ produceDot = { traversal, node_id, dotfile ->
 
   dot.close()
 }
+
+
+/*
+==>v[83636336]
+gremlin> anc.V().has('generation', 6).where(__.inE().values('dl_dist').is(4)).where(__.inE().count().is(1)).in().in()
+==>v[51605560]
+*/
+sketchAncestor = { traversal, target_id, source_id, dot_file ->
+
+  // def target = traversal.V(target_id).next()
+  Vertex source = traversal.V(source_id).next()
+  int source_generation = source.value('generation')
+  List source_genes = []
+  inject(source).out('contains').order().by('position', incr).fill(source_genes)
+
+  def subgraph = traversal.V(target_id).out('contains').repeat(
+    __.inE('creates').subgraph('sg').outV().dedup()).until(__.in('contains').has('generation', source_generation))
+  .cap('sg').next()
+
+  def subtraversal = subgraph.traversal()
+
+  // this subgraph looks like this:
+  //   *   *   *  ...  *
+  //   |   |   |  ...  |
+  //   *   *   *  ...  *
+  //   |   |   |  ...  |
+  //   :   :   :  ...  :
+  //
+  //   |   |   |       |
+  //   *   *   *  ...  *
+
+  List target_genes = []
+  traversal.V(target_id).out('contains').order().by('position',incr).fill(target_genes)
+  List target_gene_changed = (0..<(target_genes.size())).collect{false} // initialize a list so that we refer to slots by index
+
+  // find out which genes changed
+  // target_gene_changed = target_genes.collect{ gene ->
+  //   String uuid = gene.value('uuid')
+  //   subtraversal.V().has('uuid', uuid).until(values('changes').is(neq('[]'))).repeat(__.in()).count().next() != 0
+  // }
+
+  // pair the target and genes with their source (CacheVertex) and with their "changed" boolean
+
+  triples = target_genes.collect{ gene ->
+
+    String uuid = gene.value('uuid')
+    boolean changed = subtraversal.V().has('uuid', uuid).until(values('changes').is(neq('[]'))).repeat(__.in()).count().next() != 0
+
+    // Vertex parent_gene = subtraversal.V().has('uuid', uuid).until(or(values('changes').is(neq('[]')), inE().count().is(0))).repeat(__.in()).next()
+    Vertex parent_gene = subtraversal.V().has('uuid', uuid).until(inE().count().is(0)).repeat(__.in()).next()
+
+    return [gene: gene,
+            parent_gene: parent_gene,
+            changed: changed]
+  }
+
+  println(triples)
+
+  FileWriter fileWriter = new FileWriter(dot_file)
+  DotWriter dot = new DotWriter(fileWriter, -1, [:])
+
+  // draw the edges
+  triples.each{ map ->
+    parent_uuid = map['parent_gene'].value('uuid')
+    child_uuid = map['gene'].value('uuid')
+    dot.writeEdge(parent_uuid, child_uuid)
+  }
+
+
+  // group & fill out the parent nodes
+  dot.openSubgraph("cluster_0", [:])
+  for ( i = 0; i < source_genes.size() ; i++){
+    vertex = source_genes[i]
+    uuid = vertex.value('uuid')
+    position = vertex.value('position')
+    instruction = vertex.value('content')
+    dot.writeNode(uuid, [label: "\"$position, $instruction\""])
+
+    if ( i < source_genes.size() - 1){
+      next_uuid = source_genes[i+1].value('uuid')
+      dot.writeEdge(uuid, next_uuid)
+    }
+  }
+  dot.closeSubgraph()
+
+  // group & fill out the child nodes
+  dot.openSubgraph("cluster_1", [:])
+  for ( i = 0; i < triples.size(); i++) {
+    def vertex = triples[i]['gene']
+    uuid = vertex.value('uuid')
+    position = vertex.value('position')
+    instruction = vertex.value('content')
+    if ( triples[i]['changed'] == "[]") {
+      fillcolor = "white"
+    }
+    else {
+      fillcolor = "\"orange\""
+    }
+    dot.writeNode(uuid,
+                  [ label: "\"$position, $instruction\"",
+                    style: "filled",
+                    fillcolor: fillcolor])
+
+    if ( i < target_genes.size() - 1){
+      next_uuid = target_genes[i+1].value('uuid')
+      dot.writeEdge(uuid, next_uuid)
+    }
+  }
+  dot.closeSubgraph()
+
+  dot.close()
+
+}
+
+
+
